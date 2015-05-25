c9a3ff45cb30bb9d404ebf6fae2ac5c80d8f0f93
Andrew Robinson <andrew.robinson@gmail.com>
2015-05-25 06:56:09 -0700

    Adding stub RPC implementation

--- src/Server/Program.h
+++ src/Server/Program.h
@@ -5,43 +5,43 @@
 
 namespace ReSoel {
     namespace Server {
-		namespace ProcessHelpers {
+        namespace ProcessHelpers {
             void AttachConsoleHooks(HANDLE process, void* loadLibraryAddress, bool suspended);
             bool Is64BitProcess(HANDLE process);
-			void* GetLoadLibraryAddress(HANDLE process);
-			void* GetLoadLibraryAddress(HANDLE process, HANDLE kernel32FileHandle, DWORD64 baseAddress, DWORD moduleLength);
-			std::string GetFullPathOfResoelModule(const char* moduleName);
-		}
+            void* GetLoadLibraryAddress(HANDLE process);
+            void* GetLoadLibraryAddress(HANDLE process, HANDLE kernel32FileHandle, DWORD64 baseAddress, DWORD moduleLength);
+            std::string GetFullPathOfResoelModule(const char* moduleName);
+        }
 
-		class AttachProcessInjector
-		{
-		public:
-			AttachProcessInjector(DWORD pid);
-			AttachProcessInjector(SharedHandle process);
-			AttachProcessInjector(const AttachProcessInjector&) = delete;
-			SharedHandle GetProcessHandle();
+        class AttachProcessInjector
+        {
+        public:
+            AttachProcessInjector(DWORD pid);
+            AttachProcessInjector(SharedHandle process);
+            AttachProcessInjector(const AttachProcessInjector&) = delete;
+            SharedHandle GetProcessHandle();
 
-		private:
-			SharedHandle m_process;
-		};
-
-		class CreateProcessInjector
-		{
-		public:
-			CreateProcessInjector(std::string workingPath, std::string programName, std::string arguments);
-			CreateProcessInjector(const CreateProcessInjector&) = delete;
-			SharedHandle GetProcessHandle();
-
-		private:
-            SharedHandle StartProcess(const std::string& workingPath, const std::string& programName, const std::string& arguments) const;
-			struct InitialDebugInfo {
-				DWORD ProcessId;
-				DWORD ThreadId;
-				void* LoadLibraryAPtr;
-			};
-			InitialDebugInfo WaitForLoadLibraryModuleLoad() const;
+        private:
             SharedHandle m_process;
-		};
+        };
+
+        class CreateProcessInjector
+        {
+        public:
+            CreateProcessInjector(std::string workingPath, std::string programName, std::string arguments);
+            CreateProcessInjector(const CreateProcessInjector&) = delete;
+            SharedHandle GetProcessHandle();
+
+        private:
+            SharedHandle StartProcess(const std::string& workingPath, const std::string& programName, const std::string& arguments) const;
+            struct InitialDebugInfo {
+                DWORD ProcessId;
+                DWORD ThreadId;
+                void* LoadLibraryAPtr;
+            };
+            InitialDebugInfo WaitForLoadLibraryModuleLoad() const;
+            SharedHandle m_process;
+        };
 
         // Represents a child process of the ReSoel server application, with
         // its input and output console handles hooked. 
@@ -53,7 +53,7 @@ namespace ReSoel {
             ~Program();
 
         private:
-			SharedHandle m_process;
+            SharedHandle m_process;
         };
 
     }
